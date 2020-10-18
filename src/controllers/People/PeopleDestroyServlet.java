d27a8c81b2daa78ea3fbc42dc6ebb2f79c8b63f8
shunpei.kanbayashi <gkhandball1@gmail.com>
2020-10-18 19:29:16 +0900

    Add PeopleDestroyServlet

--- src/controllers/People/PeopleDestroyServlet.java
+++ src/controllers/People/PeopleDestroyServlet.java
@@ -1,19 +1,24 @@
 package controllers.People;
 
 import java.io.IOException;
+
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Person;
+import utils.DButil;
+
 /**
  * Servlet implementation class PeopleDestroyServlet
  */
 @WebServlet("/people/destroy")
 public class PeopleDestroyServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -22,11 +27,23 @@ public class PeopleDestroyServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = request.getParameter("_token");
+
+        if (_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DButil.createEntityManager();
+            Person p = em.find(Person.class, (Integer)request.getSession().getAttribute("person_id"));
+            em.getTransaction().begin();
+            em.remove(p);
+            em.getTransaction().commit();
+            em.close();
+
+            request.getSession().setAttribute("flush","削除しました");
+            response.sendRedirect(request.getContextPath() + "/admins/index.html");
+        }
+    }
 
 }
